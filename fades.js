ca9eadc00408e4a516e7e09c4c40b62012ae0e8d
NicoPicotto <nicopicotto@hotmail.com>
2023-05-14 10:43:59 -0300

    asd

--- fades.js
+++ fades.js
@@ -1,47 +1,69 @@
-function fadeIn(selector) {
-	const elements = document.querySelectorAll(`[${selector}]`);
+// Seleccionamos todos los elementos que tienen el atributo "example-fade"
+const elements = document.querySelectorAll('[example-fade]');
 
-	const fadeTransforms = {
-		s: 'translateY(15px)',
-		m: 'translateY(30px)',
-		l: 'translateY(60px)',
-	};
+// Creamos un observer para detectar cuándo los elementos están en el viewport
+const observer = new IntersectionObserver((entries, observer) => {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			const element = entry.target;
+			const fadeType = element.getAttribute('example-fade');
+			const fadeDirection = element.getAttribute('example-direction');
 
-	const fadeTransitions = {
-		s: 'opacity 400ms ease-in-out 200ms, transform 400ms ease-in-out 200ms',
-		m: 'opacity 600ms ease-in-out 300ms, transform 600ms ease-in-out 300ms',
-		l: 'opacity 800ms ease-in-out 400ms, transform 800ms ease-in-out 400ms',
-	};
+			element.style.opacity = 0;
 
-	elements.forEach((element) => {
-		const fadeType = element.getAttribute(selector);
-		element.style.opacity = 0;
-		element.style.transform = fadeTransforms[fadeType];
-		element.style.transition = fadeTransitions[fadeType];
-	});
-
-	console.log('js cargado');
-
-	const observer = new IntersectionObserver((entries, observer) => {
-		entries.forEach((entry) => {
-			if (entry.isIntersecting) {
-				const element = entry.target;
-				observer.unobserve(element);
-				fadeInElement(element);
+			switch (fadeDirection) {
+				case 'right':
+					element.style.transform = 'translateX(-15px)';
+					break;
+				case 'left':
+					element.style.transform = 'translateX(15px)';
+					break;
+				case 'top':
+					element.style.transform = 'translateY(15px)';
+					break;
+				case 'bottom':
+					element.style.transform = 'translateY(-15px)';
+					break;
+				default:
+					element.style.transform = 'translateY(15px)';
+					break;
 			}
-		});
-	});
 
+			switch (fadeType) {
+				case 's':
+					element.style.transition = 'opacity 800ms ease-in-out 200ms';
+					element.style.opacity = '0.7';
+					break;
+				case 'm':
+					element.style.transition = 'opacity 800ms ease-in-out 200ms';
+					element.style.opacity = '0.7';
+					break;
+				case 'l':
+					element.style.transition = 'opacity 1600ms ease-in-out 200ms';
+					element.style.opacity = '0.3';
+					break;
+				default:
+					break;
+			}
+
+			element.style.transition =
+				'opacity 800ms ease-in-out 200ms, transform 800ms ease-in-out 200ms';
+			element.style.opacity = 1;
+
+			observer.unobserve(element);
+		}
+	});
+});
+
+// Observamos todos los elementos
+elements.forEach((element) => {
+	observer.observe(element);
+});
+
+// Agregamos un listener para observar cuando se cargue el contenido
+document.addEventListener('DOMContentLoaded', () => {
+	// Observamos todos los elementos de nuevo en caso de que hayan sido añadidos dinámicamente
 	elements.forEach((element) => {
 		observer.observe(element);
 	});
-
-	function fadeInElement(element) {
-		element.style.opacity = 1;
-		element.style.transform = 'translateY(0)'; // regresar a la posición original
-	}
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-	fadeIn('example-fade');
 });
