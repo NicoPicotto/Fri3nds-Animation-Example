1aa4d69d768ef90d65cbb447ba13f3ca53a22c7a
NicoPicotto <nicopicotto@hotmail.com>
2023-05-14 10:53:01 -0300

    optimizacion

--- fades.js
+++ fades.js
@@ -1,69 +1,53 @@
-// Seleccionamos todos los elementos que tienen el atributo "example-fade"
-const elements = document.querySelectorAll('[example-fade]');
+function fadeIn(selector) {
+	const elements = document.querySelectorAll(`[${selector}]`);
 
-// Creamos un observer para detectar cuándo los elementos están en el viewport
-const observer = new IntersectionObserver((entries, observer) => {
-	entries.forEach((entry) => {
-		if (entry.isIntersecting) {
-			const element = entry.target;
-			const fadeType = element.getAttribute('example-fade');
-			const fadeDirection = element.getAttribute('example-direction');
+	const fadeTransforms = {
+		s: 'translate(15px)',
+		m: 'translate(30px)',
+		l: 'translate(60px)',
+	};
 
-			element.style.opacity = 0;
+	const fadeTransitions = {
+		s: 'opacity 400ms ease-in-out 200ms, transform 400ms ease-in-out 200ms',
+		m: 'opacity 600ms ease-in-out 300ms, transform 600ms ease-in-out 300ms',
+		l: 'opacity 800ms ease-in-out 400ms, transform 800ms ease-in-out 400ms',
+	};
 
-			switch (fadeDirection) {
-				case 'right':
-					element.style.transform = 'translateX(-15px)';
-					break;
-				case 'left':
-					element.style.transform = 'translateX(15px)';
-					break;
-				case 'top':
-					element.style.transform = 'translateY(15px)';
-					break;
-				case 'bottom':
-					element.style.transform = 'translateY(-15px)';
-					break;
-				default:
-					element.style.transform = 'translateY(15px)';
-					break;
-			}
+	const fadeDirections = {
+		top: 'translateY(-100%)',
+		bottom: 'translateY(100%)',
+		left: 'translateX(-100%)',
+		right: 'translateX(100%)',
+	};
 
-			switch (fadeType) {
-				case 's':
-					element.style.transition = 'opacity 800ms ease-in-out 200ms';
-					element.style.opacity = '0.7';
-					break;
-				case 'm':
-					element.style.transition = 'opacity 800ms ease-in-out 200ms';
-					element.style.opacity = '0.7';
-					break;
-				case 'l':
-					element.style.transition = 'opacity 1600ms ease-in-out 200ms';
-					element.style.opacity = '0.3';
-					break;
-				default:
-					break;
-			}
-
-			element.style.transition =
-				'opacity 800ms ease-in-out 200ms, transform 800ms ease-in-out 200ms';
-			element.style.opacity = 1;
-
-			observer.unobserve(element);
-		}
+	elements.forEach((element) => {
+		const fadeType = element.getAttribute(selector);
+		const fadeDirection = element.getAttribute('fade-direction');
+		element.style.opacity = 0;
+		element.style.transform = `${fadeTransforms[fadeType]} ${fadeDirections[fadeDirection]}`;
+		element.style.transition = fadeTransitions[fadeType];
 	});
-});
 
-// Observamos todos los elementos
-elements.forEach((element) => {
-	observer.observe(element);
-});
+	const observer = new IntersectionObserver((entries, observer) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				const element = entry.target;
+				observer.unobserve(element);
+				fadeInElement(element);
+			}
+		});
+	});
 
-// Agregamos un listener para observar cuando se cargue el contenido
-document.addEventListener('DOMContentLoaded', () => {
-	// Observamos todos los elementos de nuevo en caso de que hayan sido añadidos dinámicamente
 	elements.forEach((element) => {
 		observer.observe(element);
 	});
+
+	function fadeInElement(element) {
+		element.style.opacity = 1;
+		element.style.transform = 'translate(0)';
+	}
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	fadeIn('fade-fri3nds');
 });
